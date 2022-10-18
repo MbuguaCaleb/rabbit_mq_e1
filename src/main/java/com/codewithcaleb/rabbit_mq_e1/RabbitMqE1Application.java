a6cdeff6ea08ac1308249e39fe35218d80e59c06
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-10-18 09:53:38 +0300

    begginning-rabbit-MQ

---
+++ src/main/java/com/codewithcaleb/rabbit_mq_e1/RabbitMqE1Application.java
@@ -0,0 +1,13 @@
+package com.codewithcaleb.rabbit_mq_e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqE1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqE1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithcaleb/rabbit_mq_e1/RabbitMqE1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithcaleb.rabbit_mq_e1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqE1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
